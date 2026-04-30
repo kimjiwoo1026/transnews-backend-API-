5d46aa4796e8d2d3f862d6fce934dc2539a86b2e
kimjiwoo <jiu6631@gmail.com>
2026-04-30 19:18:12 +0900

    content 필드 추가 및 응답 규격 구체화

--- app/schemas/models.py
+++ app/schemas/models.py
@@ -1,9 +1,17 @@
-from typing import Any, Optional
-
+from typing import Any, Optional, List
 from pydantic import BaseModel
 
+class NewsItem(BaseModel):
+    title: str
+    link: str
+    article_link: str
+    original_url: str
+    source_name: str
+    source_url: Optional[str] = None
+    published: str
+    content: str  
 
 class BaseResponse(BaseModel):
     status: str
     message: str
-    data: Optional[Any] = None
+    data: Optional[List[NewsItem]] = None
